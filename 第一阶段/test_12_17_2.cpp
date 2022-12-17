de232fa06a9c606be4303df605d634e0081adecc
panguangwei <2712278005@qq.com>
2022-12-17 20:14:07 +0800

    12_17

--- 第一阶段/test_12_17_2.cpp
+++ 第一阶段/test_12_17_2.cpp
@@ -9,11 +9,13 @@ using namespace std;
 int main() {
     int a = 10;
     int b = 20;
+//    Max = 10;
     const int day = 7;
 //    day = 8;
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
     cout << "a + b = " << a + b << endl;
     cout << "Max = " << Max << endl;
+    cout << "day = " << day << endl;
     return 0;
 }
