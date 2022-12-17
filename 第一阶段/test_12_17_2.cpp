1b178d46902afd035450113fa1fa1d6bc54a8a9f
panguangwei <2712278005@qq.com>
2022-12-17 20:09:28 +0800

    12_17

--- 第一阶段/test_12_17_2.cpp
+++ 第一阶段/test_12_17_2.cpp
@@ -2,10 +2,14 @@
 // Created by 潘光伟 on 2022/12/17.
 //
 #include "iostream"
+
 using namespace std;
-int main(){
+
+int main() {
     int a = 10;
     int b = 20;
     cout << "a = " << a << endl;
+    cout << "b = " << b << endl;
+    cout << "a + b = " << a + b << endl;
     return 0;
 }
