0352931ccd756d16f25f3c7e5bbed74fa758030d
panguangwei <2712278005@qq.com>
2022-12-17 20:12:34 +0800

    12_17

--- 第一阶段/test_12_17_2.cpp
+++ 第一阶段/test_12_17_2.cpp
@@ -2,14 +2,18 @@
 // Created by 潘光伟 on 2022/12/17.
 //
 #include "iostream"
+#define Max 100
 
 using namespace std;
 
 int main() {
     int a = 10;
     int b = 20;
+    const int day = 7;
+//    day = 8;
     cout << "a = " << a << endl;
     cout << "b = " << b << endl;
     cout << "a + b = " << a + b << endl;
+    cout << "Max = " << Max << endl;
     return 0;
 }
