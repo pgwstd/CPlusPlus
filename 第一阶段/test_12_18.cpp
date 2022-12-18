9badbd7bf45717e5bc6606cd23c3b15c0e05c6e1
panguangwei <2712278005@qq.com>
2022-12-18 22:00:15 +0800

    12_18

---
+++ 第一阶段/test_12_18.cpp
@@ -0,0 +1,18 @@
+//
+// Created by 潘光伟 on 2022/12/18.
+//
+#include"iostream"
+using namespace std;
+int main()
+{
+    int a = 10;
+    short num1 = 10;
+    int num2 = 10;
+    long num3 = 10;
+    long long num4 = 10;
+    cout << a << endl;
+    cout << num1 << endl;
+    cout << num2 << endl;
+    cout << num3 << endl;
+    cout << num4 << endl;
+}
