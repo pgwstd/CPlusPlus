6b2f7e448531423c641d40a488f4bc3c66f2c182
panguangwei <2712278005@qq.com>
2022-12-19 21:09:47 +0800

    12_19

--- 第一阶段/test_12_19.cpp
+++ 第一阶段/test_12_19.cpp
@@ -2,9 +2,14 @@
 // Created by 潘光伟 on 2022/12/19.
 //
 #include "iostream"
+#include "string"
 using namespace std;
 int main()
 {
+    char str[] = "hello";//c语言风格
+    string str2 = "hello";//c++语言风格(要包括头文件)
+    cout << str2 <<endl;
+    cout << str << endl;
     cout << "hello world\t" << endl;
     cout << "aaa\thello world" << endl;
     cout << "a\thello world" << endl;
