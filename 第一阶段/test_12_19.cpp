12e315ed6e5ef4433b6044c3fd94942a987cd361
panguangwei <2712278005@qq.com>
2022-12-19 20:56:16 +0800

    12_19

---
+++ 第一阶段/test_12_19.cpp
@@ -0,0 +1,11 @@
+//
+// Created by 潘光伟 on 2022/12/19.
+//
+#include "iostream"
+using namespace std;
+int main()
+{
+    cout << "hello world\t" << endl;
+    cout << "\\a" << endl;
+    return 0;
+}
