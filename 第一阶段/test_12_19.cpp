1c617141d5d1024fc9202226d95db29dc31a9cd1
panguangwei <2712278005@qq.com>
2022-12-19 21:36:10 +0800

    12_18

--- 第一阶段/test_12_19.cpp
+++ 第一阶段/test_12_19.cpp
@@ -3,24 +3,37 @@
 //
 #include "iostream"
 #include "string"
+
 using namespace std;
-int main()
-{
-    char str[] = "hello";//c语言风格
-    string str2 = "hello";//c++语言风格(要包含头文件)
-    int input = 0;
-    cin >> input;
-    cout << input <<endl;
-    bool flag = true;
-    bool flag2 = false;
-    cout << flag <<endl;
-    cout << flag2 << endl;
-    cout << str2 <<endl;
-    cout << str << endl;
-    cout << "hello world\t" << endl;
-    cout << "aaa\thello world" << endl;
-    cout << "a\thello world" << endl;
-    cout << "aa\thello world" << endl;
-    cout << "\\a" << endl;
+
+int main() {
+
+    bool b = false;
+    cin >> b;
+    cout << b << endl;
+//    char str[] = "hello";//c语言风格
+//    string str2 = "hello";//c++语言风格(要包含头文件)
+//    cin >> str2;
+//    cout << str2 << endl;
+//    char a = 'a';
+//    cin >> a;
+//    cout << a << endl;
+//    int input = 0;
+//    float input2 = 0;
+//    cin >> input;
+//    cin >> input2;
+//    cout << input << endl;
+//    cout << input2 << endl;
+//    bool flag = true;
+//    bool flag2 = false;
+//    cout << flag << endl;
+//    cout << flag2 << endl;
+//    cout << str2 << endl;
+//    cout << str << endl;
+//    cout << "hello world\t" << endl;
+//    cout << "aaa\thello world" << endl;
+//    cout << "a\thello world" << endl;
+//    cout << "aa\thello world" << endl;
+//    cout << "\\a" << endl;
     return 0;
 }
