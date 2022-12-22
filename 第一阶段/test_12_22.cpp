e1c173bd65ceb15746986385434c43b5344b2427
panguangwei <2712278005@qq.com>
2022-12-22 21:43:33 +0800

    12_22

--- 第一阶段/test_12_22.cpp
+++ 第一阶段/test_12_22.cpp
@@ -5,6 +5,18 @@
 using namespace std;
 int main()
 {
-
+    int num = rand() % 100 + 1;
+    int input = 0;
+    while (true) {
+        cin >> input;
+        if (input > num) {
+            cout << "大了" << endl;
+        } else if (input < num) {
+            cout << "小了" << endl;
+        } else {
+            cout << "猜对了" << endl;
+            break;
+        }
+    }
     return 0;
 }
