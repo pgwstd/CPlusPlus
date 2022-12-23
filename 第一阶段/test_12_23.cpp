3136723925cc8473e16f948eecf03e72fda64a26
panguangwei <2712278005@qq.com>
2022-12-23 22:01:18 +0800

    12_23

---
+++ 第一阶段/test_12_23.cpp
@@ -0,0 +1,13 @@
+//
+// Created by 潘光伟 on 2022/12/23.
+//
+#include "iostream"
+
+using namespace std;
+
+int main() {
+    int a = 10;
+    int b = 20;
+    cout << (a > b ? a : b) << endl;
+    return 0;
+}
