68253b706d5814a44c87f4997a292c1cb94c8c63
panguangwei <2712278005@qq.com>
2022-12-25 21:50:00 +0800

    12_25

--- 第一阶段/test_12_25.cpp
+++ 第一阶段/test_12_25.cpp
@@ -2,13 +2,25 @@
 // Created by 潘光伟 on 2022/12/25.
 //
 #include "iostream"
+
 using namespace std;
-int main()
-{
-    int n = 0;
+
+int main() {
+    /* int n = 0;
+     do {
+         cout << n << endl;
+         n++;
+     } while (n < 10);*/
+
+    int num = 100;
     do {
-        cout << n << endl;
-        n++;
-    } while (n < 10);
+        int a = num % 10;
+        int b = num / 10 % 10;
+        int c = num / 100;
+        if (a * a * a + b * b * b + c * c * c == num) {
+            cout << num << endl;
+        }
+        num++;
+    } while (num < 1000);
     return 0;
 }
