44ceb2092ef2d297a5800c4c4624a2618f3652e1
panguangwei <2712278005@qq.com>
2022-12-25 21:37:36 +0800

    12_25

---
+++ 第一阶段/test_12_25.cpp
@@ -0,0 +1,14 @@
+//
+// Created by 潘光伟 on 2022/12/25.
+//
+#include "iostream"
+using namespace std;
+int main()
+{
+    int n = 0;
+    do {
+        cout << n << endl;
+        n++;
+    } while (n < 10);
+    return 0;
+}
