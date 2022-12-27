4144028ca9cbfec910e66cf6c8ff647fc51392c2
panguangwei <2712278005@qq.com>
2022-12-27 20:26:01 +0800

    12_27

--- 第一阶段/test_12_27.cpp
+++ 第一阶段/test_12_27.cpp
@@ -2,9 +2,20 @@
 // Created by 潘光伟 on 2022/12/27.
 //
 #include "iostream"
-using namespace std;
-int main(){
 
+using namespace std;
+
+int main() {
+
+    for (int i = 1; i <= 9; ++i) {
+        for (int j = 1; j <= 9; ++j) {
+            cout << i << " * " << j << " = " << i * j << "\t";
+            if (i == j) {
+                break;
+            }
+        }
+        cout << endl;
+    }
 
 
     return 0;
