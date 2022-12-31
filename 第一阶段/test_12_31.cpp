d673b66c0fe1a487b12cbc0a362f84a4bee12da1
panguangwei <2712278005@qq.com>
2022-12-31 13:34:43 +0800

    12_31

--- 第一阶段/test_12_31.cpp
+++ 第一阶段/test_12_31.cpp
@@ -2,13 +2,23 @@
 // Created by 潘光伟 on 2022/12/31.
 //
 #include "iostream"
+
 using namespace std;
-int main(){
-    for (int i = 1; i <= 100; ++i) {
-        if (i % 3 == 0){
-            continue;
-        }
-        cout << i << endl;
-    }
+
+int main() {
+//    for (int i = 1; i <= 100; ++i) {
+//        if (i % 3 == 0){
+//            continue;
+//        }
+//        cout << i << endl;
+//    }
+    cout << "1" << endl;
+    cout << "2" << endl;
+    goto Frit;
+    cout << "3" << endl;
+    cout << "4" << endl;
+
+    Frit:
+    cout << "5" << endl;
     return 0;
 }
