c979c43e02893509058e1679d653da5f0c1fd26c
panguangwei <2712278005@qq.com>
2023-01-01 23:31:31 +0800

    1_1

--- 第一阶段/test_1_1.cpp
+++ 第一阶段/test_1_1.cpp
@@ -6,15 +6,14 @@ using namespace std;
 int main(){
     int array[5] = {100,200,80,98,250};
     int a = sizeof (array)/sizeof (array[0]);
+    int max = array[0];
     for (int i = 0; i < a; ++i) {
-        if (array[i] < array[i+1]){
-            int temp = array[i];
-            array[i] = array[i+1];
-            array[i+1] = temp;
+        if (array[i] > max){
+            max = array[i];
         }
     }
-    for (int j = 0; j < a; ++j) {
-        cout << array[j] << endl;
-    }
+
+        cout << max << endl;
+
     return 0;
 }
