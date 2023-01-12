00cf67628e9cdf45b7cd692eae7d6b52f631947f
panguangwei <2712278005@qq.com>
2023-01-12 21:43:30 +0800

    1_12

--- 第一阶段/test_1_12.cpp
+++ 第一阶段/test_1_12.cpp
@@ -2,12 +2,14 @@
 // Created by 潘光伟 on 2023/1/12.
 //
 #include "iostream"
+
 using namespace std;
-int main(){
+
+int main() {
     int a = 10;
     int b = 12;
     const int *d = &b;//常量指针
-    int * const e = &a;
+    int *const e = &a;
     int *p = &a;
     cout << "sizeof (int *) = " << sizeof(int *) << endl;
     return 0;
