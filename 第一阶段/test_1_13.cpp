1be857bedb8b355f5a3be591d393f3112362d39b
panguangwei <2712278005@qq.com>
2023-01-13 18:56:58 +0800

    1_13

---
+++ 第一阶段/test_1_13.cpp
@@ -0,0 +1,18 @@
+//
+// Created by 潘光伟 on 2023/1/13.
+//
+#include "iostream"
+
+using namespace std;
+//比较三个整数，输出他们的最大值
+int maxNumber(int x, int y, int z) {
+    return 1;
+}
+
+int main() {
+    int a, b, c;
+    cin >> a >> b >> c ;
+    maxNumber(a, b, c);
+    cout << maxNumber << endl;
+    return 0;
+}
