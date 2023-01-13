4782d7215cf34a9b9ac721ae1469287bafc9eb07
panguangwei <2712278005@qq.com>
2023-01-13 20:04:43 +0800

    1_13

--- 第一阶段/test_1_5.cpp
+++ 第一阶段/test_1_5.cpp
@@ -5,7 +5,20 @@
 
 using namespace std;
 
-int main() {
+struct Student{
+    string name;
+    int age;
+    int score;
+};
 
+int main() {
+    struct Student s1;
+    s1.name = "李华";
+    s1.age = 21;
+    s1.score = 98;
+    struct Student s2 = {"小明",21,99};
+    cout << "姓名:" << s1.name << endl;
+    cout << "年龄:" << s1.age << endl;
+    cout << "成绩:" << s1.score << endl;
     return 0;
 }
