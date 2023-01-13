d8f76de2865d1ae9b51bc12bdb5d7af708512f3c
panguangwei <2712278005@qq.com>
2023-01-13 19:01:26 +0800

    1_13

--- 第一阶段/test_1_13.cpp
+++ 第一阶段/test_1_13.cpp
@@ -7,13 +7,24 @@ using namespace std;
 
 //比较三个整数，输出他们的最大值
 int maxNumber(int x, int y, int z) {
-    return 1;
+    int max = 0;
+    if (x > y && x > z){
+        max = x;
+    }
+    if (y > x && y > z){
+        max = y;
+    }
+    if (z > x && z > y){
+        max = z;
+    }
+    return max;
 }
 
 int main() {
     int a, b, c;
+    cout << "请输入三个整数:";
     cin >> a >> b >> c;
     int max = maxNumber(a, b, c);
-    cout << max << endl;
+    cout << "最大值为:" << max << endl;
     return 0;
 }
