f5f20b555435064a9de0fc6c179e12dd745f44b5
panguangwei <2712278005@qq.com>
2023-01-13 19:07:29 +0800

    1_13

---
+++ 第一阶段/test_1_4.cpp
@@ -0,0 +1,17 @@
+//
+// Created by 潘光伟 on 2023/1/13.
+//
+#include "iostream"
+
+using namespace std;
+
+//冒泡排序
+void BubbleSort(int *arr,int size){
+
+}
+int main() {
+    int arr[] = {3, 2, 56, 24, 12345, 73, 124, 14, 5435, 879, 98756};
+    int len = sizeof(arr) / sizeof(arr[0]);
+    BubbleSort(&arr, len);
+    return 0;
+}
