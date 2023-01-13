31cdf822bf0039761b9c4d57d92a1a8503b79e37
panguangwei <2712278005@qq.com>
2023-01-13 19:12:32 +0800

    1_13

--- 第一阶段/test_1_4.cpp
+++ 第一阶段/test_1_4.cpp
@@ -10,7 +10,7 @@ void BubbleSort(int *arr, int size) {
     int tmp = 0;
     for (int i = 0; i < size - 1; ++i) {
         for (int j = 0; j < size - i - 1; ++j) {
-            if (arr[j] > arr[j + 1]){
+            if (arr[j] > arr[j + 1]) {
                 tmp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = tmp;
@@ -18,15 +18,17 @@ void BubbleSort(int *arr, int size) {
         }
     }
 }
-void Printf(int * arr, int size){
+
+void Printf(int *arr, int size) {
     for (int i = 0; i < size; ++i) {
         cout << arr[i] << endl;
     }
 }
+
 int main() {
     int arr[] = {3, 2, 56, 24, 12345, 73, 124, 14, 5435, 879, 98756};
     int len = sizeof(arr) / sizeof(arr[0]);
     BubbleSort(arr, len);
-    Printf(arr,len);
+    Printf(arr, len);
     return 0;
 }
